68617ca8793fdd6102b0eeda164a7ee2f99dcc01
Danil Chizhikov <chizhikov.danil@inbox.ru>
2024-05-24 18:49:16 +0700

    complete run project

--- Assets/Scripts/Core/StateMachine/Runtime/States/BootstrapState.cs
+++ Assets/Scripts/Core/StateMachine/Runtime/States/BootstrapState.cs
@@ -3,7 +3,7 @@ using Cysharp.Threading.Tasks;
 
 namespace TestTask.StateMachine.States
 {
-    internal sealed class BootstrapState : IState
+    internal sealed class BootstrapState : IBootstrapState
     {
         public async UniTask EnterAsync(CancellationToken token)
         {
--- Assets/Scripts/Core/StateMachine/Runtime/States/MenuState.cs
+++ Assets/Scripts/Core/StateMachine/Runtime/States/MenuState.cs
@@ -3,7 +3,7 @@ using Cysharp.Threading.Tasks;
 
 namespace TestTask.StateMachine.States
 {
-    internal sealed class MenuState : IState
+    internal sealed class MenuState : IMenuState
     {
         public async UniTask EnterAsync(CancellationToken token)
         {
