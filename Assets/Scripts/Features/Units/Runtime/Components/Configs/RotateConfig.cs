191fabc98ffef6d94b61a3f11f135d641c254e78
Danil Chizhikov <chizhikov.danil@inbox.ru>
2024-05-25 10:34:25 +0700

    add rotate config to unit

--- Assets/Scripts/Features/Units/Runtime/Components/Configs/RotateConfig.cs
+++ Assets/Scripts/Features/Units/Runtime/Components/Configs/RotateConfig.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 namespace TestTask.Units
 {
+    [CreateAssetMenu(menuName = "Configs/Units/Components/" + nameof(RotateConfig), fileName = nameof(RotateConfig))]
     internal sealed class RotateConfig : ComponentConfig, IRotateConfig
     {
         [SerializeField, Min(0f)] private float _speed = 0f;
